abbb9d48b47fc1a1d45791a9b8f09bc362177846
baruto0217-cpu <baruto0217@gmail.com>
2026-02-24 12:20:06 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App' // 拡張子 .jsx をあえて書かない設定
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
